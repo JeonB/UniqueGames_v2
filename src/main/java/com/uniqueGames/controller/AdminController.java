997b5c3217df7093e241542eafe4c541ec0f7b24
SongYuna99 <thddbsk7248@gmail.com>
2023-07-06 18:44:39 +0900

    admin 관련 파일 생성 order 관련 페이지 header,footer CSS 적용

---
+++ src/main/java/com/uniqueGames/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.uniqueGames.controller;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+    @RequestMapping(value = "admin")
+    public String admin(){
+        return "admin/admin";
+    }
+}
