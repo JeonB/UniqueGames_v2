0d919d5837c194cabb4267b47ac35283cb207a8e
Corendo6 <durrl5897@gmail.com>
2023-07-18 18:45:12 +0900

    오류수정, 에디터 적용 시작 notice-mapper.xml:쿼리문 오류 수정 NoticeController.java, RestNoticeController.java: 에디터 적용 시작

--- src/main/java/com/uniqueGames/controller/RestNoticeController.java
+++ src/main/java/com/uniqueGames/controller/RestNoticeController.java
@@ -1,19 +1,31 @@
 package com.uniqueGames.controller;
 
 import com.uniqueGames.config.Login;
+ import com.uniqueGames.fileutil.FileUploadUtil;
 import com.uniqueGames.model.Comment;
 import com.uniqueGames.model.Company;
 import com.uniqueGames.service.CommentService;
 import com.uniqueGames.service.NoticeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-public class RestNoticeController {
+@Slf4j
+public class RestNoticeController extends FileUploadUtil {
 
     private final CommentService commentService;
     private final NoticeService noticeService;
+
+    @Override
+    protected void extractFile(Object obj) {
+        setFile((MultipartFile) obj);
+    }
 
     /**
      * 댓글 작성 처리
@@ -43,5 +55,15 @@ public class RestNoticeController {
 
 
         return noticeService.deleteList(list, company);
+    }
+
+    @PostMapping("/test")
+    public Map<String, Object> test(@RequestParam Map<String, Object> paramMap, MultipartRequest request) {
+        String fileName = fileCheck(request.getFile("upload"));
+        fileSave();
+        log.info(getFile().toString());
+        log.info(getImageName());
+        paramMap.put("url", getROOT_PATH() + fileName);
+        return paramMap;
     }
 }
