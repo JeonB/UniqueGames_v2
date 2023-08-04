75af091206f89e7005dc61a6950e38c1bc9bc0b2
JeonB <newjeonb@gmail.com>
2023-08-04 21:51:53 +0900

    JPA 기초 CRUD 메소드 테스트 코드 추가 IntroJPARepository: custom method add -> findByName, findByNameAndContent IntroJPARepositoryTest:Add CRUD Test Methods

--- src/test/java/com/uniqueGames/repository/IntroJPARepositoryTest.java
+++ src/test/java/com/uniqueGames/repository/IntroJPARepositoryTest.java
@@ -1,12 +1,9 @@
 package com.uniqueGames.repository;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.uniqueGames.model.Intro;
 import java.util.List;
 import java.util.Optional;
-import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
