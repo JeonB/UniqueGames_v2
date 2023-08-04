2e4a4cbba97d3ce0fd04482daa92ccd27214b205
JeonB <newjeonb@gmail.com>
2023-08-04 21:51:14 +0900

    JPA 기초 CRUD 메소드 테스트 코드 추가 IntroJPARepository: custom method add -> findByName, findByNameAndContent IntroJPARepositoryTest:Add CRUD Test Methods

--- src/main/java/com/uniqueGames/repository/IntroJPARepository.java
+++ src/main/java/com/uniqueGames/repository/IntroJPARepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IntroJPARepository extends JpaRepository<Intro, Integer> {
 
+    Intro findByName(String name);
+
+    Intro findByNameAndContent(String name,String content);
 }
--- src/test/java/com/uniqueGames/repository/IntroJPARepositoryTest.java
+++ src/test/java/com/uniqueGames/repository/IntroJPARepositoryTest.java
@@ -1,9 +1,11 @@
 package com.uniqueGames.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.uniqueGames.model.Intro;
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +17,61 @@ class IntroJPARepositoryTest {
     private IntroJPARepository introJPARepository;
 
     @Test
-    void testJpa() {
+    void testJpaSave() {
+        // Create
         Intro intro = new Intro();
-        intro.setName("sbb가 무엇인가요?");
-        intro.setContent("sbb에 대해서 알고 싶습니다.");
-        this.introJPARepository.save(intro);  // 첫번째 질문 저장
+        intro.setCId("test");
+        intro.setName("가보자가보자");
+        intro.setContent("일단 박고 시작하는 가보자가보자입니다");
+        this.introJPARepository.save(intro);
+    }
+    @Test
+    void testJpaFindAll(){
+        // Read
+        List<Intro> all = this.introJPARepository.findAll();
+        assertEquals(5, all.size());
+        Intro q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getName());
+    }
 
-        Intro intro2 = new Intro();
-        intro2.setName("스프링부트 모델 질문입니다.");
-        intro2.setContent("id는 자동으로 생성되나요?");
-        this.introJPARepository.save(intro2);  // 두번째 질문 저장
+    @Test
+    void testJpaFindById() {
+        Optional<Intro> oq = this.introJPARepository.findById(1);
+        if(oq.isPresent()) {
+            Intro q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getName());
+        }
+    }
+
+    @Test
+    void testJpaFindByName() {
+        Intro i = this.introJPARepository.findByName("베스트게이밍");
+        assertEquals(5,i.getId());
+    }
+
+    @Test
+    void testJpaFindByNameAndContent() {
+        Intro i = this.introJPARepository.findByNameAndContent("베스트게이밍",
+                "언제나 새로운 희망을 펼쳐나가는 베스트게이밍입니다");
+        assertEquals(5,i.getId());
+    }
+
+    @Test
+    void testJpaUpdate() {
+        Optional<Intro> oq = this.introJPARepository.findById(1);
+        assertTrue(oq.isPresent());
+        Intro q = oq.get();
+        q.setName("수정된 제목");
+        this.introJPARepository.save(q);
+    }
+
+    @Test
+    void testJpaDelete() {
+        assertEquals(5, this.introJPARepository.count());
+        Optional<Intro> oq = this.introJPARepository.findById(1);
+        assertTrue(oq.isPresent());
+        Intro q = oq.get();
+        this.introJPARepository.delete(q);
+        assertEquals(4, this.introJPARepository.count());
     }
 }
