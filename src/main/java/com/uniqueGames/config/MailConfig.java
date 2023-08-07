f9cce249d93b9295c8796e5d58e1d27d73e29def
Corendo6 <durrl5897@gmail.com>
2023-08-08 00:50:50 +0900

    보안 강화

--- src/main/java/com/uniqueGames/config/MailConfig.java
+++ src/main/java/com/uniqueGames/config/MailConfig.java
@@ -1,7 +1,10 @@
 package com.uniqueGames.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -10,13 +13,16 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Autowired
+    private Environment environment;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("a01083529494@gmail.com");
-        mailSender.setPassword("tohd zfgq phan zzrg");
+        mailSender.setUsername(environment.getProperty("EMAIL_ADDRESS"));
+        mailSender.setPassword(environment.getProperty("EMAIL_PASSWORD"));
 
         Properties mailProperties = new Properties();
         mailProperties.put("mail.transport.protocol", "smtp");
